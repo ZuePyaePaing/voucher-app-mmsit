c33057b4363f3d17ab969ebda8a9fb435ca27e9a
ZuePyaePaing <kaunglay24588@gmail.com>
2024-10-28 18:40:37 -0700

    update code

--- day-14/src/pages/DashboardPage.jsx
+++ day-14/src/pages/DashboardPage.jsx
@@ -1,33 +1,39 @@
 import React from "react";
-import { Database, Files, MonitorCheck } from "lucide-react";
+import { Database, Files, MonitorCheck, UserRoundPen } from "lucide-react";
 import Container from "../components/Container";
 import ModuleBtn from "../components/ModuleBtn";
 
-
 const Dashboard = () => {
   return (
-    <section className="w-full min-h-screen">
+    <section className="w-full h-screen">
       <Container className={` space-y-3`}>
         <div className=" grid grid-cols-3 gap-5">
           <div className=" col-span-3  md:col-span-1">
             <ModuleBtn
               name={"Product"}
               icon={<Database size={35} />}
-              url={"/product"}
+              url={"/dashboard/product"}
             />
           </div>
           <div className=" col-span-3  md:col-span-1">
             <ModuleBtn
               name={"Sale"}
               icon={<MonitorCheck size={35} />}
-              url={"/sale"}
+              url={"/dashboard/sale"}
             />
           </div>
           <div className=" col-span-3  md:col-span-1">
             <ModuleBtn
               name={"Voucher"}
               icon={<Files size={35} />}
-              url={"/voucher"}
+              url={"/dashboard/voucher"}
+            />
+          </div>
+          <div className=" col-span-3  md:col-span-1">
+            <ModuleBtn
+              name={"Profile"}
+              icon={<UserRoundPen size={35} />}
+              url={"/dashboard/profile"}
             />
           </div>
         </div>
